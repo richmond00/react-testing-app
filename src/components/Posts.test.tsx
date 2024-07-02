117c6663bb3383ce781c276fa45d83aa00be2701
Dongwook Kim <dongwook227@gmail.com>
2024-07-02 19:46:25 -0400

    add test cases

---
+++ src/components/Posts.test.tsx
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react';
+
+describe('When posts is loading from server', () => {
+    test('should display error ui', () => {
+
+    });
+});
+
+describe('When posts failed to load from server', () => {
+    test('should display error ui', () => {
+
+    });
+});
+
+describe('When posts is loaded', () => {
+    test('should display posts', () => {
+
+    });
+});
+
+describe('When user is in the first page', () => {
+    test('should disable Previous page button', () => {
+
+    });
+});
+
+describe('When user clicks title of post', () => {
+    test('should display selected post', () => {
+
+    });
+});
+
+describe('When user clicks Next page button', () => {
+    test('should move to next page', () => {
+
+    });
+});
+
+describe('When user clicks Previous page button', () => {
+    test('should move to previous page', () => {
+
+    });
+});
+
+describe('When user is in the last page', () => {
+    test('should disable Next page button', () => {
+
+    });
+});

4439461fd2e800dca55a6e0d31f885337a690183
Dongwook Kim <dongwook227@gmail.com>
2024-07-02 19:33:24 -0400

    add unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,18 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test.skip("renders learn react link", () => {
-  render(<App />);
+jest.mock('./components/Posts', () => ({
+  Posts: () => <div data-testid="posts-component">Posts</div>
+}))
+
+describe('App', () => {
+  test("should display page title", () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: 'Blog Posts' })).toBeInTheDocument();
+  });
+
+  test("should render Posts component", () => {
+    render(<App />);
+    expect(screen.getByTestId('posts-component')).toBeInTheDocument();
+  });
 });
